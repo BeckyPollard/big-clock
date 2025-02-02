1fcd4956e102a06fb68f70b75b61e3650a9233b9
BeckyPollard <becky-pollard@hotmail.com>
2025-02-01 21:24:12 -0500

    Passing options to BigClock

--- src/scripts/App.tsx
+++ src/scripts/App.tsx
@@ -3,42 +3,22 @@ import {Options} from '../types/types'
 import BigClock from './components/BigClock';
 import BigClockOptions from './components/BigClockOptions';
 
-
-
-
 function App() {
-  const [options, setOptions] = useState<Options>({announceHour: false});
+  const initOptions: Options = {
+    announceHour: false,
+  } 
+  const [options, setOptions] = useState<Options>({...initOptions});
   
-  const handleOptions = () => {
-    setOptions(options);
+  const handleOptions = (newOpts: Options) => {
+    setOptions(newOpts);
   }
   
   return (
     <>
-      <BigClock />
+      <BigClock announceHour={options.announceHour}/>
       <BigClockOptions options={options} handleOptionsChange={handleOptions} />
     </>
   );
 }
-
-
-
-{/*
-
-  SKETCH OF APP:
-  BIG CLOCK
-  OPTIONS
-    - ALARM
-    - NOISE
-    - COLOURS
-    - IN-15-MIN TIME SUBTITLE
-
-  <input
-    type='radio'
-    value='value'
-    onChange={(e) => setSelectedOption(e.target.value)}
-  />
-
-*/}
 
 export default App;
--- src/scripts/components/BigClock.tsx
+++ src/scripts/components/BigClock.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import {useState} from 'react';
 
-// type BigClockProps = {
-
-// };
-export default function BigClock() {
-  const [time, setTime] = useState('BIG CLOCK');
+type BigClockProps = {
+  announceHour: boolean;
+};
+export default function BigClock(props: BigClockProps) {
+  const now = new Date()
+    .toLocaleTimeString([], {timeStyle: 'short'})
+    .replace(/[.]/g, '');
+  const [time, setTime] = useState(now);
 
   const UpdateTime = () => {
-    const date = new Date().toLocaleTimeString();
-    setTime(date);
-    // smells bad
+    const newTime = new Date()
+      .toLocaleTimeString([], {timeStyle: 'short'})
+      .replace(/[.]/g, '');
+    setTime(newTime);
   };
-
-  setInterval(UpdateTime);
+  setInterval(UpdateTime, 1000);
+  // talk to your doctor and see if useInterval is right for you.
+  // further reading: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+  // https://www.geeksforgeeks.org/how-to-use-setinterval-method-inside-react-components/
 
   return (
     <main>
