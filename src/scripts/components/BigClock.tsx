171d1dfd82a4c2c5398a62610895e56ee3042b25
BeckyPollard <becky-pollard@hotmail.com>
2025-02-18 15:13:00 -0500

    Announce time on the hour check

--- src/scripts/components/BigClock.tsx
+++ src/scripts/components/BigClock.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 type BigClockProps = {
   announceHour: boolean;
@@ -8,18 +8,54 @@ export default function BigClock(props: BigClockProps) {
   const now = new Date()
     .toLocaleTimeString([], {timeStyle: 'short'})
     .replace(/[.]/g, '');
-  const [time, setTime] = useState(now);
+  
+    const [time, setTime] = useState(now);
 
-  const UpdateTime = () => {
-    const newTime = new Date()
+  useEffect(() => {
+    // TIME KEEPING!
+
+    const interval = setInterval(() => {
+      const newTime = new Date()
       .toLocaleTimeString([], {timeStyle: 'short'})
       .replace(/[.]/g, '');
-    setTime(newTime);
-  };
-  setInterval(UpdateTime, 1000);
-  // talk to your doctor and see if useInterval is right for you.
-  // further reading: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
-  // https://www.geeksforgeeks.org/how-to-use-setinterval-method-inside-react-components/
+      if (time !== newTime) {
+        setTime(newTime);
+
+        // ANNOUNCE TIME
+        if (props.announceHour) {
+          if (newTime.includes('1:00')) {
+            console.log('DING DONG 1 o clock', newTime);
+          } else if (newTime.includes('2:00')) {
+            console.log('DING DONG 2 o clock', newTime);
+          } else if (newTime.includes('3:00')) {
+            console.log('DING DONG 3 o clock', newTime);
+          } else if (newTime.includes('4:00')) {
+            console.log('DING DONG 4 o clock', newTime);
+          } else if (newTime.includes('5:00')) {
+            console.log('DING DONG 5 o clock', newTime);
+          } else if (newTime.includes('6:00')) {
+            console.log('DING DONG 6 o clock', newTime);
+          } else if (newTime.includes('7:00')) {
+            console.log('DING DONG 7 o clock', newTime);
+          } else if (newTime.includes('8:00')) {
+            console.log('DING DONG 8 o clock', newTime);
+          } else if (newTime.includes('9:00')) {
+            console.log('DING DONG 9 o clock', newTime);
+          } else if (newTime.includes('10:00')) {
+            console.log('DING DONG 10 o clock', newTime);
+          } else if (newTime.includes('11:00')) {
+            console.log('DING DONG 11 o clock', newTime);
+          } else if (newTime.includes('12:00')) {
+            console.log('DING DONG 12 o clock', newTime);
+          }
+          // ew
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+    //https://www.geeksforgeeks.org/how-to-use-setinterval-method-inside-react-components/
+  }, [time]);
 
   return (
     <main>
