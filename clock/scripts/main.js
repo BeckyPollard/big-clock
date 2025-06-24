757039cf235cbc81c5a5af93c726bc6578755936
BeckyPollard <becky-pollard@hotmail.com>
2025-06-24 01:05:12 -0400

    Fix AM/PM

--- clock/scripts/main.js
+++ clock/scripts/main.js
@@ -121,7 +121,8 @@ const renderClock = () => {
   let hr = `${(date.getHours() > 12) ? date.getHours() - 12 : date.getHours()}`;
   let min = `:${(date.getMinutes() < 10) ? "0" + date.getMinutes() : date.getMinutes()}`;
   let sec = `:${(date.getSeconds() < 10) ? "0" + date.getSeconds() : date.getSeconds()}`;
-  let meridiem = options.meridiem ? (hour > 12) ? " AM" : " PM" : '';
+  let meridiem = options.meridiem ? (date.getHours() < 12) ? " AM" : " PM" : '';
+  console.log(date.getHours());
   // midnight check
   if(hr == 0) {
     hr = 12;
